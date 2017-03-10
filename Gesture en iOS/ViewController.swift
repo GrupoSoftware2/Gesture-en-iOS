5083839a46af816bda0697fc598eeba909996bb6
alumno <alumno@a51-03.local>
2017-03-09 21:34:32 -0500

    Primer commit

--- Gesture en iOS/ViewController.swift
+++ Gesture en iOS/ViewController.swift
@@ -10,9 +10,50 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var tipo:String!
+    
+    @IBOutlet weak var contenedor: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        print(tipo)
+        
+        switch tipo {
+        case "UITapGestureRecognizer":
+            let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(tap))
+            tapGestureRecognizer.numberOfTapsRequired = 2
+            contenedor.addGestureRecognizer(tapGestureRecognizer)
+        case "UIPinchGestureRecognizer":
+            let pinchGestureRecognizer = UIPinchGestureRecognizer(target: self, action: #selector(pinch))
+            pinchGestureRecognizer.scale = 2
+            contenedor.addGestureRecognizer(pinchGestureRecognizer)
+        case "UIRotationGestureRecognizer":
+            let rotationGestureRecognizer = UIRotationGestureRecognizer(target: self, action: #selector(rotation))
+            //rotationGestureRecognizer.rotation = 2
+            contenedor.addGestureRecognizer(rotationGestureRecognizer)
+        case "UISwipeGestureRecognizer":
+            let swipeGestureRecognizer = UISwipeGestureRecognizer(target: self, action: #selector(swipe))
+            swipeGestureRecognizer.direction = UISwipeGestureRecognizerDirection.right
+            swipeGestureRecognizer.numberOfTouchesRequired = 2
+            contenedor.addGestureRecognizer(swipeGestureRecognizer)
+        case "UIPanGestureRecognizer":
+            let panGestureRecognizer = UIPanGestureRecognizer(target: self, action: #selector(pan))
+            panGestureRecognizer.maximumNumberOfTouches = 3
+            panGestureRecognizer.minimumNumberOfTouches = 1
+            contenedor.addGestureRecognizer(panGestureRecognizer)
+        case "UIScreenEdgePanGestureRecognizer" :
+            let screenEdgePanGestureRecognizer = UIScreenEdgePanGestureRecognizer(target: self, action: #selector(screenEdgePan))
+            screenEdgePanGestureRecognizer.edges = UIRectEdge.right
+            self.view.addGestureRecognizer(screenEdgePanGestureRecognizer)
+        default:
+            let longPressGestureRecognizer = UILongPressGestureRecognizer(target: self, action: #selector(longPress))
+            longPressGestureRecognizer.minimumPressDuration = 2
+            longPressGestureRecognizer.numberOfTapsRequired = 0
+            longPressGestureRecognizer.numberOfTouchesRequired = 1
+            contenedor.addGestureRecognizer(longPressGestureRecognizer)
+        }
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +61,40 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func tap(){
+        print("tap")
+    }
+    
+    func pinch(sender:UIPinchGestureRecognizer){
+        print("pinch")
+        print("Scale: \(sender.scale)")
+        print("Velocidad: \(sender.velocity)")
+    }
+    
+    func rotation(sender:UIRotationGestureRecognizer){
+        print("rotation")
+        print("rotatio: \(sender.rotation)")
+        print("velocidad: \(sender.velocity)")
+    }
+    
+    func swipe (sender:UISwipeGestureRecognizer){
+        print("swipe")
+        //print("direction: \(sender.direction)")
+    }
+    
+    func pan (sender:UIPanGestureRecognizer){
+        print("pan")
+        print("maximum: \(sender.maximumNumberOfTouches)")
+        print("minimum: \(sender.minimumNumberOfTouches)")
+    }
+    
+    func screenEdgePan(){
+        print("screenEdgePan")
+    }
+    
+    func longPress(){
+        print("longPress")
+    }
 
 }
 
